2516c942c8962bd342abe1f536f19d70d08423b6
Ariel Mirra <ariel.mirra@ing.austral.edu.ar>
2017-05-17 20:11:50 -0300

    added exercise 2

---
+++ TPCuatrimestral/Exercise2/User.h
@@ -0,0 +1,20 @@
+#ifndef CFROMJAVA_USER_H
+#define CFROMJAVA_USER_H
+
+typedef struct User{
+    char* name;
+    char* dni;
+    char* address;
+    char* telephone;
+    char* city;
+    char* province;
+    char* country;
+    char* postalCode;
+    //int rol;
+
+}User;
+
+//functions
+
+
+#endif //CFROMJAVA_USER_H
